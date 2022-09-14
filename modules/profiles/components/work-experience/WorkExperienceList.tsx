83104201f662e810d82f01a2db5f13422b3c2073
Lahiru Dilshan <lahiru4unew4@gmail.com>
2022-09-15 03:07:33 +0530

    fix: description UI issue

--- modules/profiles/components/work-experience/WorkExperienceList.tsx
+++ modules/profiles/components/work-experience/WorkExperienceList.tsx
@@ -5,7 +5,7 @@ import { DeleteOutlined, ExpandOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 import { TWorkExperience } from '@modules/profiles/types/work-experience/entity';
 import { dateFormat } from '@shared/utils';
-import { Space } from '@shared/styles';
+import { Flex, Space } from '@shared/styles';
 
 const WorkExperienceList = ({ data, mode = 'edit', onAction }: TWorkExperienceListProps) => {
     return (
@@ -35,11 +35,11 @@ const WorkExperienceList = ({ data, mode = 'edit', onAction }: TWorkExperienceLi
                                 }
                             />
                             {mode === 'edit' && (
-                                <>
+                                <Flex>
                                     <Button icon={<ExpandOutlined />} type='primary' onClick={() => onAction && onAction({ action: 'edit', data: item })} ghost>Edit</Button>
                                     <Space left={1} />
                                     <Button icon={<DeleteOutlined />} onClick={() => onAction && onAction({ action: 'delete', data: item })} danger>Delete</Button>
-                                </>
+                                </Flex>
                             )}
                         </List.Item>
                     )}
@@ -66,6 +66,9 @@ const WorkExperienceListContainer = styled.div`
         height: 40px;
         line-height: 39px;
         border-radius: 8px; 
+    }
+    .ant-list-item-meta-description {
+        margin-right: 20px;
     }
 `;
 
